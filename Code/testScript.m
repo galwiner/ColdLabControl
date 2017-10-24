52a7eeadad2673b4da94ae04eefa577f6d56b5e3
galwiner <galwiner@gmail.com>
2017-10-24 18:46:29 +0300

    enabled triggered analog read functionality  (function 10 0 in the host)

--- Code/testScript.m
+++ Code/testScript.m
@@ -1,20 +1,33 @@
+% 
 clear all
 close all
 clc
-
+basicImports
+seqUpload(UnloadMotSeq(channelTable));
 prog=CodeGenerator();
-prog.GenPause(1e6);
 prog.GenLodingMeasTrig;
-prog.GenFinish();
+seq=LoadMotSeq(channelTable,0,100);
+prog.GenSeq(seq);
+prog.GenFinish;
 prog.DisplayCode();
-
 com=Tcp2Labview('localhost',6340);
 com.UploadCode(prog);
 com.UpdateFpga;
 com.WaitForHostIdle;
 com.Execute(1);
-dat=com.readMemoryBlock(1500,100);
+com.WaitForHostIdle;
+pause(10)
+dat=com.readMemoryBlock(1000,999);
+
+dat=dat*10/2^15;
+
 com.Delete;
+
 
 figure;
 plot(dat)
+
+foldername=
+filename=
+
+save(dat,
