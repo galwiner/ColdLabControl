62165223ac68f3e1f00699eebeffcc3855fbf0c0
galwiner <galwiner@gmail.com>
2017-10-31 18:30:40 +0200

    added scope object and customsave func

--- Code/InstrumentScripts/Scope/keysightScope.m
+++ Code/InstrumentScripts/Scope/keysightScope.m
@@ -10,19 +10,55 @@ classdef keysightScope < handle
     methods
         function obj = keysightScope(scopename)
             %KEYSIGHTSCOPE Construct an instance of this class
-            %   Detailed explanation goes here
+            
             obj.scopename=scopename;
             obj.sc = visa('agilent',obj.scopename);
-            obj.sc.InputBufferSize = 20000;
+            obj.sc.InputBufferSize = 200000;
             obj.sc.Timeout = 20;
             obj.sc.ByteOrder = 'littleEndian';
             fopen(obj.sc);
+            fprintf(obj.sc,'*RST');
             fprintf(obj.sc,':WAVEFORM:SOURCE CHAN1');
             fprintf(obj.sc,':WAV:POINTS:MODE NORMAL');
-            fprintf(obj.sc,':WAV:POINTS 10000');
+            fprintf(obj.sc,':WAV:POINTS 100000');
+        end
+        function setTimebase(obj,time)
+            fprintf(obj.sc,[':TIMebase:RANGe ' num2str(time)]);
+        end
+        
+        function setDelay(obj,time)
+            fprintf(obj.sc,[':TIMebase:DELay ' num2str(time)]);
+        end
+        
+        function setState(obj,state)
+            if strcmpi(state,'run')
+                fprintf(obj.sc,':RUN');
+            elseif strcmpi(state,'stop')
+                fprintf(obj.sc,':stop');
+            elseif strcmpi(state,'single')
+                fprintf(obj.sc,':single');
+            else
+                error('no such state');
+                
+            end
+            
+        end
+        
+        function clearState(obj)
+            fprintf(obj.sc,'*CLS');
         end
         
         function [x,y]=getChan(obj,chanum)
+            
+            %             if length(chanum)==1
+            %                 fprintf(obj.sc,[':WAVEFORM:SOURCE CHAN' num2str(chanum)]);
+            %             else
+            %                 for i=1:length(chanum)
+            %                     fprintf(obj.sc,[':WAVEFORM:SOURCE CHAN' num2str(chanum(i))]);
+            %                 end
+            %             end
+            obj.setChan(chanum,1);
+            fprintf(obj.sc,[':WAV:SOURCE CHANNEL' num2str(chanum)]);
             fprintf(obj.sc,[':DIGITIZE CHAN' num2str(chanum)]);
             
             % Wait till complete
@@ -39,7 +75,10 @@ classdef keysightScope < handle
             
             preambleBlock = query(obj.sc,':WAVEFORM:PREAMBLE?');
             
+            
+                
             fprintf(obj.sc,':WAV:DATA?');
+            
             % read back the BINBLOCK with the data in specified format and store it in
             % the waveform structure. FREAD removes the extra terminator in the buffer
             obj.waveform.RawData = binblockread(obj.sc,'uint16'); fread(obj.sc,1);
@@ -76,6 +115,87 @@ classdef keysightScope < handle
             x = (obj.waveform.XIncrement.*(1:length(obj.waveform.RawData))) - obj.waveform.XIncrement;
             y = (obj.waveform.YIncrement.*(obj.waveform.RawData - obj.waveform.YReference)) + obj.waveform.YOrigin;
             
+        end
+        
+        
+        function plotChan(obj,chanum)
+            figure;
+            
+            [x,y]=obj.getChan(chanum);
+            plot(x,y);
+            xlabel('T[s]');
+            ylabel('V[V]');
+            title([obj.scopename ' ' datestr(now)]);
+            
+        end
+        
+        function dat=getChannels(obj)
+            dat=[];
+            for i=1:4
+            [x,y]=obj.getChan(i);
+            dat=horzcat(dat,x',y);
+            end
+            
+            
+        end
+        
+        function getScreen(obj)
+            figure;
+            hold on
+            for i=1:4
+            [x,y]=obj.getChan(i);
+            plot(x,y,'o');
+            end
+            xlabel('T[s]');
+            ylabel('V[V]');
+            title([obj.scopename ' ' datestr(now)]);
+            
+        end
+        
+        function out=SCPISend(obj,msg)
+            fprintf(obj.sc,msg);
+            %     out=fscanf(obj.sc);
+        end
+        
+        function labelChan(obj,chanum,label)
+            fprintf(obj.sc,[':CHANnel' num2str(chanum) ':LABel "' label '"']);
+        end
+        
+        function setOffset(obj,chanum,offset)
+            fprintf(obj.sc,[':CHANnel' num2str(chanum) ':OFFSet ' num2str(offset) 'V']);
+            
+        end
+        
+        function setProbeRatio(obj,chanum,ratio)
+            fprintf(obj.sc,[':CHANnel' num2str(chanum) ':PROBe ' num2str(ratio)]);
+        end
+        function setVrange(obj,chanum,range)
+            fprintf(obj.sc,[':CHANnel' num2str(chanum) ':RANGe ' num2str(range) 'V']);
+        end
+        
+        function setDVM(obj,state)
+            fprintf(obj.sc,['DVM:ENABLE ' state]);
+        end
+        
+        function setTrigger(obj,chanum,level,slope)
+            if strcmpi(slope,'POS') || strcmpi(slope,'NEG') || strcmpi(slope,'EITHER')
+                fprintf(obj.sc,['TRIGGER:MODE EDGE']);
+                fprintf(obj.sc,['TRIGGER:EDGE:SLOPE ' slope]);
+            else
+                error('slope has to be POS, NEG or EITHER');
+            end
+            if strcmpi(chanum,'EXT')
+                fprintf(obj.sc,['TRIGGER:EDGE:SOURCE EXT']);
+            else
+                fprintf(obj.sc,['TRIGGER:EDGE:SOURCE CHANNEL' num2str(chanum)]);
+            end
+            fprintf(obj.sc,['TRIGGER:EDGE:LEVEL ' num2str(level)]);
+            
+        end
+        
+        function setChan(obj,chanum,state)
+            
+            fprintf(obj.sc,[':CHANNEL' num2str(chanum) ':DISPLAY ' num2str(state)]);
         end
         
         function delete(obj)
